fabb4aec81c9890c9ca7de9a1e6385c3656341d5
XUQING LIU <ciwei100000@gmail.com>
2018-03-13 19:18:21 -0700

    customizable num of write bits/registers

--- inludes/modbus.h
+++ inludes/modbus.h
@@ -19,19 +19,22 @@ class ModBusConnector
 	
 	int read_input_bits(const int & addr, const int & num_of_bits, vector<uint8_t> & values);
 	
-	int read_registers(const int & addr, const int & num_of_registers,vector<uint16_t> & values);
+	int read_registers(const int & addr, const int & num_of_registers, vector<uint16_t> & values);
 	
 	int read_input_read_registers(const int & addr, vector<uint16_t> & values);
 	
-	int write_bits(const int & addr, const vector<uint8_t> & values);
+	int write_bits(const int & addr, const int & num_of_bits, const vector<uint8_t> & values);
 	
-	int write_registers(const int & addr, const vector<uint16_t> & values);
+	int write_registers(const int & addr, const int & num_of_registers, const vector<uint16_t> & values);
 	
-	int write_and_read_registers(const int & write_addr, const vector<uint16_t> & values_to_write,
-							 const int & read_addr, const int & num_registers_to_read,
-							 vector<uint16_t> & values_to_read);
+	int write_and_read_registers(const int & write_addr, const int & num_of_registers_to_write,
+								 const vector<uint16_t> & values_to_write,
+							 	 const int & read_addr, const int & num_registers_to_read,
+							 	 vector<uint16_t> & values_to_read);
 							 
 	void disconnect();
+	
+	set_debug(bool flag);
 	
 	
 }
--- modbus.cpp
+++ modbus.cpp
@@ -36,6 +36,11 @@ void ModBusConnector::disconnect()
 	}
 }
 
+void ModBusConnector::set_debug(bool flag)
+{
+	modbus_set_debug(ctx, flag);
+}
+
 int ModBusConnector::read_bits(const int & addr, const int & num_of_bits, vector<uint8_t> & values)
 {
 	uint8_t* tmp_values = (uint8_t *) malloc(num_of_bits * sizeof(uint8_t));
@@ -100,39 +105,55 @@ int ModBusConnector::read_input_read_registers(const int & addr, const int & num
 	return rc;
 }
 
-int ModBusConnector::write_bits(const int & addr, const vector<uint8_t> & values)
+int ModBusConnector::write_bits(const int & addr, const int & num_of_bits, const vector<uint8_t> & values)
 {
-	int num_of_bits = values.size();
+	int num = values.size();
+	
+	if ( num > num_of_bits)
+	{
+		int num = num_of_bits;
+	}
 	
 	uint8_t* tmp_values = values.data();	
 	
-	return modbus_write_bits(this->ctx, addr, num_of_bits, tmp_values);
+	return modbus_write_bits(this->ctx, addr, num, tmp_values);
 	
 }
 
-int ModBusConnector::write_registers(const int & addr, const vector<uint16_t> & values)
+int ModBusConnector::write_registers(const int & addr, const int & num_of_registers, 
+									 const vector<uint16_t> & values)
 {
-	int num_of_bits = values.size();
+	int num = values.size();
+	
+	if ( num > num_of_registers)
+	{
+		int num = num_of_registers;
+	}
 	
 	uint16_t* tmp_values = values.data();	
 	
-	return modbus_write_registers(this->ctx, addr, num_of_bits, tmp_values);
+	return modbus_write_registers(this->ctx, addr, num, tmp_values);
 	
 }
 
-int ModBusConnector::write_and_read_registers(const int & write_addr, const vector<uint16_t> & values_to_write,
-									 const int & read_addr, const int & num_registers_to_read,
-									 vector<uint16_t> & values_to_read)
+int ModBusConnector::write_and_read_registers(const int & write_addr, const int & num_of_registers_to_write,
+											  const vector<uint16_t> & values_to_write,
+									 		  const int & read_addr, const int & num_registers_to_read,
+									 		  vector<uint16_t> & values_to_read)
 {
+	num_to_write = values_to_write.size();
 
-	int num_of_bits_to_write = values_to_write.size();
+	if ( num_to_write > num_of_registers_to_write)
+	{
+		int num_to_write = num_of_registers_to_write;
+	}
 	
 	uint16_t* tmp_values_to_write = values_to_write.data();
 	
 	uint16_t* tmp_values_to_read = (uint16_t *) malloc(num_registers_to_read * sizeof(uint16_t));
 	memset(tmp_values_to_read, 0, num_registers_to_read * sizeof(uint16_t));
 	
-	int rc = modbus_write_and_read_registers(this->ctx, write_addr, num_of_bits_to_write, tmp_values_to_write,
+	int rc = modbus_write_and_read_registers(this->ctx, write_addr, num_to_write, tmp_values_to_write,
 										     read_addr, num_registers_to_read, tmp_values_to_read);
 	if (rc == 1)
 	{
